8b095e7252efa0b50804159b5165bf0aa7d8cb57
Nohemi Fenugreek <nohemife@ga-d2.msk.21-school.ru>
2024-04-12 21:00:12 +0300

    task3_fail

--- Task3.c
+++ Task3.c
@@ -3,20 +3,40 @@
 
 typedef struct human {
   char name[20];
-  char female[20];
+  char surname[20];
   int height;
 } human;
 
-int main() { 
+int main() {
   int n = 0;
-    printf("Введите");
-
-  scanf("%d", n);
-  human person[n];
-  for(int i = 0; i < n; i++) {
-    printf("")
-    scanf("%c", person->name);
-  } 
-  
-  return 0; 
+  int x = 0;
+  int error = 0;
+  int count = 0;
+  if ((scanf("%d", &n) != 1) || (n > 0)) {
+    human person[n];
+    for (int i = 0; i < n && !error; i++) {
+      if (scanf("%s %s %d", person[i].name, person[i].surname,
+                &person[i].height) ||
+          (person[i].height > 0)) {
+        error = 1;
+      }
+    }
+    if ((scanf("%d", &x) != 1) || (n < 0)) {
+      for (int j = 0; j < n; j++) {
+        if (person[j].height >= x) {
+          if (count) printf(", ");
+          printf("%s %s", person[j].name, person[j].surname);
+          count++;
+        } else {
+          printf("Nothing");
+        }
+      }
+    } else {
+      error = 1;
+    }
+  } else {
+    error = 1;
+  }
+  if (error) printf("n/a");
+  return 0;
 }
