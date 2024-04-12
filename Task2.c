b6c8672d6584a4575af19517cf6ed296d17396fd
Nohemi Fenugreek <nohemife@ga-d2.msk.21-school.ru>
2024-04-12 20:31:31 +0300

    task2

---
+++ Task2.c
@@ -0,0 +1,17 @@
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+  int r = 0;
+  int p = 21500;
+  double pi = M_PI;
+  printf("Введите радиус шара: ");
+  scanf("%d", &r);
+
+  if (r >= 0) {
+    printf("%0.lf", (p * (4.0 / 3.0) * pi * pow(r, 3)));
+  } else {
+    printf("n/a");
+  }
+  return 0;
+}
---
+++ Task3.c
@@ -0,0 +1,16 @@
+#include <math.h>
+#include <stdio.h>
+
+typedef struct human {
+  char name[20];
+  char female[20];
+  int height;
+} human;
+
+int main() { 
+  int n = 0;
+  scanf("%d", n);
+  human 
+  
+  return 0; 
+}
